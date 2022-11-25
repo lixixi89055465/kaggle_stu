d7144492665262b61c153209276a152e3bdd0d60
lixixi89055465 <nanji1850094299@outlook.com>
2022-11-25 11:49:06 +0800

    Handling Missing Values

---
+++ Data Cleaning/Exercise - Scaling and Normalization.py
@@ -0,0 +1,38 @@
+'''
+'''
+import pandas as pd
+import numpy as np
+from scipy import stats
+from mlxtend.preprocessing import minmax_scaling
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+kickstarters_2017 = pd.read_csv("./input/kickstarter-projects/ks-projects-201801.csv")
+# set seed for reproducibility
+np.random.seed(0)
+
+original_data = pd.DataFrame(kickstarters_2017.usd_goal_real)
+scaled_data = minmax_scaling(original_data, columns=['usd_goal_real'])
+# print('Original data \n Preview :\n', original_data.head())
+# print(
+#     'Minimum value:', float(original_data.min()),
+#     '\nMaximum value:', float(original_data.max()) )
+# print('_'*30)
+# print('\n Scaled data \n Preview :\n',scaled_data.head())
+# print('Minimum value:',float(scaled_data.min()),
+#       '\nMaximum value:', float(scaled_data.max()))
+
+original_goal_data = pd.DataFrame(kickstarters_2017.goal)
+scaled_goal_data = minmax_scaling(original_goal_data, columns=['goal'])
+
+# print(kickstarters_2017.columns)
+index_of_positive_pledges=kickstarters_2017.usd_pledged_real>0
+positive_pledges=kickstarters_2017.usd_pledged_real.loc[index_of_positive_pledges]
+normalized_pledges=pd.Series(stats.boxcox(positive_pledges)[0],name='usd_pledged_real',index=positive_pledges.index)
+
+##
+index_of_positive_pledges=kickstarters_2017.pledged>0
+positive_pledges=kickstarters_2017.pledged.loc[index_of_positive_pledges]
+normalized_pledges=pd.Series(stats.boxcox(positive_pledges)[0],name='pledged',index=positive_pledges.index)
+
---
+++ Data Cleaning/Scaling and Normalization.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+from scipy import stats
+from mlxtend.preprocessing import minmax_scaling
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+
+original_data = np.random.exponential(size=1000)
+scaled_data = minmax_scaling(original_data, columns=[0])
+# fig, ax = plt.subplots(1, 2, figsize=(15, 3))
+# sns.histplot(original_data, ax=ax[0], kde=True, legend=False)
+# ax[0].set_title("Original Data")
+# sns.histplot(scaled_data, ax=ax[1], kde=True, legend=False)
+# ax[1].set_title("Scaled data")
+# plt.show()
+
+normalized_data=stats.boxcox(original_data)
+
+fig, ax = plt.subplots(1, 2, figsize=(15, 3))
+sns.histplot(original_data, ax=ax[0], kde=True, legend=False)
+ax[0].set_title("Original Data")
+sns.histplot(normalized_data, ax=ax[1], kde=True, legend=False)
+ax[1].set_title("normalized_data ")
+plt.show()
