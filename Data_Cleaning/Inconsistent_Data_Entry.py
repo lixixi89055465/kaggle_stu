b84b2744e9d1253a552fff73fce5ac4b112cc39a
nanji <1850094299@qq.com>
2022-11-28 11:55:32 +0800

    kaggle

--- Data_Cleaning/Inconsistent_Data_Entry.py
+++ Data_Cleaning/Inconsistent_Data_Entry.py
@@ -15,9 +15,19 @@ print(professors.head())
 countries = professors['Country'].unique()
 countries.sort()
 print(countries)
+print('0'*100)
 professors['Country'] = professors['Country'].str.lower()
 professors['Country'] = professors['Country'].str.strip()
 
-countries=professors['Country'].unique()
+countries = professors['Country'].unique()
 countries.sort()
 print(countries)
+print('1'*100)
+
+matches = fuzzywuzzy.process.extract('south korea',
+                                     countries,
+                                     limit=10,
+                                     scorer=fuzzywuzzy.fuzz.token_sort_ratio)
+print(matches)
+print('2'*100)
+
