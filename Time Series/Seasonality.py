b760c33bbea2211d81663f47852285bb45a67ca5
nanji <1850094299@qq.com>
2023-02-24 13:14:09 +0800

    init

---
+++ Time Series/Seasonality.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+
+
+def fourier_features(index, freq, order):
+    time = np.arange(len(index), dtype=np.float32)
+    k = 2 * np.pi * (1 / freq) * time
+    features = {}
+    for i in range(1, order + 1):
+        features.update({
+            f"sin_{freq}_{i}": np.sin(i * k)
+        })
+    return pd.DataFrame(features, index=index)
+
+
+from pathlib import Path
+from warnings import simplefilter
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+simplefilter('ignore')
+
+# Set Matplotlib defaults
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True, figsize=(11, 5))
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=16,
+    titlepad=10,
+)
+plot_params = dict(
+    color="0.75",
+    style=".-",
+    markeredgecolor="0.25",
+    markerfacecolor="0.25",
+    legend=False,
+)
+
+
+# %config InlineBackend.figure_format = 'retina'
+
+# annotations: https://stackoverflow.com/a/49238256/5769929
+def seasonal_plot(X, y, period, freq, ax=None):
+    if ax is None:
+        _, ax = plt.subplots()
+    palette = sns.color_palette("husl", n_colors=X[period].nunique(), )
+    ax = sns.lineplot(
+        x=freq,
+        y=y,
+        hue=period,
+        data=X,
+        ci=False,
+        ax=ax,
+        palette=palette,
+        legend=False,
+    )
+    ax.set_title(f"Seasonal Plot ({period}/{freq})")
+    for line, name in zip(ax.lines, X[period].unique()):
+        y_ = line.get_ydata()[-1]
+        ax.annotate(
+            name,
+            xy=(1, y_),
+            xytext=(6, 0),
+            color=line.get_color(),
+            xycoords=ax.get_yaxis_transform(),
+            textcoords="offset points",
+            size=14,
+            va="center",
+        )
+    return ax
+
+
+def plot_periodogram(ts, detrend='linear', ax=None):
+    from scipy.signal import periodogram
+    fs = pd.Timedelta('1Y') / pd.Timedelta('1D')
+    frequence, spectrum = periodogram(
+        ts,
+        fs=fs,
+        detrend=detrend
+    )
+
+
+data_dir = Path("../input/ts-course-data")
+tunnel = pd.read_csv(data_dir / 'tunnel.csv', parse_dates=['Day'])
+tunnel = tunnel.set_index('Day').to_period('D')
+print(tunnel.head())
+
+X = tunnel.copy()
+X['day'] = X.index.dayofweek
+X['week'] = X.index.week
+X['dayofyear']=X.index.dayofyear
+X['year']=X.index.year
+
+fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(11, 6))
+seasonal_plot(X, y="NumVehicles", period="week", freq="day", ax=ax0)
+seasonal_plot(X, y="NumVehicles", period="year", freq="dayofyear", ax=ax1);
+plt.show()
