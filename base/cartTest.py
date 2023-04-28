f01f6d68cdc885f5763f2fb9ee30e2e368014132
lixixi89055465 <1850094299@qq.com>
2023-04-28 22:16:30 +0800

    Trend

--- base/cartTest.py
+++ base/cartTest.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 from sklearn import datasets
 
@@ -78,3 +78,26 @@ plt.scatter(X[y == 1, 0], X[y == 1, 1])
 plt.show()
 # 「决策树这种非参数学习很容易过拟合，所以在实际使用这些参数的时候，要注意避免决策树模型被过渡调节参数，从而导致决策树模型欠拟合。同时这些参数并不是相互独立的，它们之间可以相互组合，所以可以使用网格搜索的方式寻找最优的参数组合。」
 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+boston = datasets.load_boston()
+X = boston.data
+y = boston.target
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=666)
+from sklearn.tree import DecisionTreeRegressor
+
+dt_reg = DecisionTreeRegressor()
+dt_reg.fit(X_train, y_train)
+
+# DecisionTreeRegressor(criterion='mse', max_depth=None, max_features=None,
+#                               max_leaf_nodes=None, min_impurity_decrease=0.0,
+#                               min_impurity_split=None, min_samples_leaf=1,
+#                               min_samples_split=2, min_weight_fraction_leaf=0.0,
+#                               presort=False, random_state=None, splitter='best')
+print(dt_reg.score(X_test, y_test))
+
+print(dt_reg.score(X_train, y_train))
