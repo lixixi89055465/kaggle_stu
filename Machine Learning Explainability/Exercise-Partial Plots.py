f02fa4c49f78e73c774145b0789e240068990615
lixixi89055465 <1850094299@qq.com>
2023-04-29 19:28:02 +0800

    Trend

--- Machine Learning Explainability/Exercise-Partial Plots.py
+++ Machine Learning Explainability/Exercise-Partial Plots.py
@@ -42,7 +42,7 @@ PartialDependenceDisplay.from_estimator(first_model, val_X, f_names, ax=ax)
 print("2" * 100)
 feat_name = 'pickup_longitude'
 PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
-plt.show()
+# plt.show()
 
 data['abs_lon_change'] = abs(data.dropoff_longitude - data.pickup_longitude)
 data['abs_lat_change'] = abs(data.dropoff_latitude - data.pickup_latitude)
@@ -59,6 +59,76 @@ new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(X, y, random_s
 second_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(new_train_X, new_train_y)
 feat_name = 'pickup_longitude'
 disp = PartialDependenceDisplay.from_estimator(second_model, new_val_X, [feat_name])
-plt.show()
-print("4"*100)
+# plt.show()
+print("4" * 100)
+for feat_name in base_features:
+    PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+    # plt.show()
+# Add your code here
+# f_names = [('pickup_longitude', 'dropoff_longitude')]
+# PartialDependenceDisplay.from_estimator(first_model, val_X, f_names, ax=ax)
+# plt.show()
 
+# feat_name = 'pickup_longitude'
+# PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+# plt.show()
+
+# Your code here
+# create new features
+data['abs_lon_change'] = abs(data.dropoff_longitude - data.pickup_longitude)
+data['abs_lat_change'] = abs(data.dropoff_latitude - data.pickup_latitude)
+
+features_2 = ['pickup_longitude',
+              'pickup_latitude',
+              'dropoff_longitude',
+              'dropoff_latitude',
+              'abs_lat_change',
+              'abs_lon_change']
+
+X = data[features_2]
+new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(X, y, random_state=1)
+second_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(new_train_X, new_train_y)
+feat_name = 'pickup_longitude'
+disp = PartialDependenceDisplay.from_estimator(second_model, new_val_X, [feat_name])
+# plt.show()
+
+from numpy.random import rand
+import numpy as np
+
+n_samples = 20000
+
+X1 = 4 * rand(n_samples) - 2
+X2 = 4 * rand(n_samples) - 2
+y = np.ones(n_samples)
+my_df = pd.DataFrame({
+    'X1': X1,
+    'X2': X2,
+    'y': y
+})
+predictors_df = my_df.drop(['y'], axis=1)
+my_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(predictors_df, my_df.y)
+disp = PartialDependenceDisplay.from_estimator(my_model, predictors_df, ['X1'])
+plt.show()
+
+import eli5
+from eli5.sklearn import PermutationImportance
+
+n_samples = 20000
+
+# Create array holding predictive feature
+X1 = 4 * rand(n_samples) - 2
+X2 = 4 * rand(n_samples) - 2
+# Create y. you should have X1 and X2 in the expression for y
+y = X1 * X2
+
+# create dataframe because pdp_isolate expects a dataFrame as an argument
+my_df = pd.DataFrame({'X1': X1, 'X2': X2, 'y': y})
+predictors_df = my_df.drop(['y'], axis=1)
+
+my_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(predictors_df, my_df.y)
+
+disp = PartialDependenceDisplay.from_estimator(my_model, predictors_df, ['X1'], grid_resolution=300)
+
+perm = PermutationImportance(my_model).fit(predictors_df, my_df.y)
+eli5.show_weights(perm, feature_names = ['X1', 'X2'])
+plt.show()
