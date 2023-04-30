ba6d7a4225b1fd5c1f05c92caae7dc88a8aaa599
lixixi89055465 <1850094299@qq.com>
2023-04-30 23:07:41 +0800

    SHAP Values

---
+++ Machine Learning Explainability/SHAP Values.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+data = pd.read_csv('../input/fifa-2018-match-statistics/FIFA 2018 Statistics.csv')
+print(data.columns)
+y = (data['Man of the Match'] == 'Yes')
+feature_names = [i for i in data.columns if data[i].dtype in [np.int64]]
+X = data[feature_names]
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+my_model = RandomForestClassifier(random_state=0).fit(train_X, train_y)
