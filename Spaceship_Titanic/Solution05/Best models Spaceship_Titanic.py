169664d9f7690ab42188ad5d1128b0dfce8613d8
lixixi89055465 <1850094299@qq.com>
2023-07-17 19:55:53 +0800

    Spaceship_Titanic\Solution05\Best models Spaceship_Titanic.py
    init

--- Spaceship_Titanic/Solution05/Best models Spaceship_Titanic.py
+++ Spaceship_Titanic/Solution05/Best models Spaceship_Titanic.py
@@ -1,3 +1,36 @@
 '''
 Hello and Welcome to Kaggle, the online Data Science Community to learn, share, and compete. Most beginners get lost in the field, because they fall into the black box approach, using libraries and algorithms they don't understand. This tutorial will give you a 1-2-year head start over your peers, by providing a framework that teaches you how-to think like a data scientist vs what to think/code. Not only will you be able to submit your first competition, but you’ll be able to solve any problem thrown your way. I provide clear explanations, clean code, and plenty of links to resources. Please Note: This Kernel is still being improved. So check the Change Logs below for updates. Also, please be sure to upvote, fork, and comment and I'll continue to develop. Thanks, and may you have "statistically significant" luck!
 '''
+import sys
+
+print('Python version:{}'.format(sys.version))
+import pandas as pd
+
+print("Pandas version:{}".format(pd.__version__))
+import matplotlib
+
+print('matplotlib.version {}'.format(matplotlib.__version__))
+
+import numpy as np
+
+print("Numpy version {}".format(np.__version__))
+
+import scipy as sp
+
+print("scipy version: {}".format(sp.__version__))
+
+import IPython
+from IPython import display
+
+print("Ipython version :{} ".format(IPython.__version__))
+
+import sklearn
+
+print('Sklear version: {}'.format(sklearn.__version__))
+
+import random
+import time
+
+import warnings
+warnings.filterwarnings('ignore')
+print('-'*25)
