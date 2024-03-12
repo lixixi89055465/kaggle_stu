670aec57baa563eed40bb53a2f2954a040b6985d
xiang.li4 <wb_xiang.li4@sinitek.com>
2024-03-12 15:03:10 +0800

    kaggle

--- Spaceship_Titanic/Solution05/Best models Spaceship_Titanic.py
+++ Spaceship_Titanic/Solution05/Best models Spaceship_Titanic.py
@@ -676,15 +676,16 @@ MLA = [
 	# Ensemble Methods
 	ensemble.AdaBoostClassifier(),
 	ensemble.BaggingClassifier(),
-	# ensemble.ExtraTreesClassifier(),
-	# ensemble.GradientBoostingClassifier(),
+	ensemble.ExtraTreesClassifier(),
+	#TODO
+	ensemble.GradientBoostingClassifier(),
 	# ensemble.RandomForestClassifier(),
 	#
 	# # Gaussian Processes
 	# gaussian_process.GaussianProcessClassifier(),
 	# # GLM
 	# linear_model.LogisticRegressionCV(),
-	# linear_model.PassiveAggressiveClassifier(),
+	 linear_model.PassiveAggressiveClassifier(),
 	# linear_model.RidgeClassifierCV(),
 	# linear_model.SGDClassifier(),
 	# linear_model.Perceptron(),
