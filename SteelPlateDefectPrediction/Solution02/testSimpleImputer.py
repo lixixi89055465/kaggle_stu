434969273dbdb3b0dee0db7862289675cf144197
xiang.li4 <wb_xiang.li4@sinitek.com>
2024-04-13 12:09:08 +0800

    kaggle_stu

--- SteelPlateDefectPrediction/Solution02/testSimpleImputer.py
+++ SteelPlateDefectPrediction/Solution02/testSimpleImputer.py
@@ -7,3 +7,49 @@
 # @Comment :
 from sklearn.impute import SimpleImputer
 import numpy as np
+
+X = np.array([[1, 2, 3],
+			  [4, 5, 6],
+			  [7, 8, 9]])
+X1 = np.array([[1, 2, np.nan],
+			   [4, np.nan, 6],
+			   [np.nan, 8, 9]])
+imp=SimpleImputer(missing_values=np.nan,strategy='mean')
+imp.fit(X)
+print(imp.transform(X1))
+print('0'*100)
+X1 = np.array([[1, 2, np.nan],
+			   [4, np.nan, 6],
+			   [np.nan, 8, 9]])
+
+imp=SimpleImputer(missing_values=np.nan,strategy='mean')
+print(imp.fit_transform(X1))
+print('1'*100)
+print(imp.get_params())
+print('2'*100)
+
+X = np.array([[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]])
+
+imp = SimpleImputer(missing_values=1, strategy='constant', fill_value=666)
+print(imp.fit_transform(X))
+print('3'*100)
+X1 = np.array([[1, 2, np.nan],
+               [4, np.nan, 6],
+               [np.nan, 8, 9]])
+print('4'*100)
+
+imp = SimpleImputer(missing_values=np.nan, strategy='mean', add_indicator=True)
+X1 = imp.fit_transform(X1)
+print(X1)
+print(imp.inverse_transform(X1))
+print('5'*100)
+
+X = np.array([[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]])
+
+imp = SimpleImputer(missing_values=1, strategy='constant', fill_value=None)
+print(imp.fit_transform(X))
+
