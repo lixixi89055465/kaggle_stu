6a5c3925b4fa9ae271a0867bc2f0f1b0579cc6d7
lixixi89055465 <nanji1850094299@outlook.com>
2024-05-06 23:01:46 +0800

    kaggle

---
+++ SteelPlateDefectPrediction/Solution02/testPowerTransformer.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/5/6 22:08
+# @Author : nanji
+# @Site :https://blog.csdn.net/m0_55894587/article/details/130474769
+# @File : testPowerTransformer.py
+# @Software: PyCharm 
+# @Comment : 
+import warnings
+
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
+
+df = pd.read_csv('../input/heart.csv')
+print(df.isna().sum())
+df['gender'] = LabelEncoder().fit_transform(df['gender'])
+print(df['class'].value_counts())
+
+print(df['class'].value_counts())
+# 对数据进行简单的过采样
+df_1 = df[df['class'] == 1]
+# df = pd.concat([df, df_1])
+# df = pd.concat([df, df_1])
+print('0' * 100)
+print(df['class'].value_counts())
+
+X = df.iloc[:, :-1]  # 特征列
+Y = df.iloc[:, -1:].values.ravel()  # 目标列
+print(X.shape)
+print(Y.shape)
+
+# 直接划分训练集与测试集进行分类
+X_train, X_test, y1_train, y1_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+gnb1 = GaussianNB()
+gnb1.fit(X_train, y1_train)
+pre = gnb1.predict(X_test)
+pre = pre.astype(np.int64)
+acc_score = round(accuracy_score(pre, y1_test), 4)
+print('未进行正态化处理的准确率：{}'.format(acc_score))
+
+# # 对数据进行正态化处理
+from sklearn.preprocessing import PowerTransformer
+powerTransformer=PowerTransformer()
+powerTransformer.fit(X)
+px=powerTransformer.transform(X)
+px_train,px_test,y2_train,y2_test=train_test_split(px,Y,\
+												   test_size=0.2, \
+												   random_state=0)
+# 对正态化处理的数据进行分类
+
+gnb2=GaussianNB()
+gnb2.fit(px_train,y2_train)
+pre=gnb2.predict(px_test)
+pre=pre.astype(np.int64)
+acc_score=round(accuracy_score(pre,y2_test),4)
+
+print('进行正态化处理准确率：{}'.format(acc_score))
--- SteelPlateDefectPrediction/Solution02/testSelectFromModel01.py
+++ SteelPlateDefectPrediction/Solution02/testSelectFromModel01.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
-# @Time : 2024/5/1 22:22
+# @Time : 2024/5/6 21:27
 # @Author : nanji
-# @Site : 
-# @File : testSelectFromModel.py
+# @Site :https://zhuanlan.zhihu.com/p/141506312
+# @File : testSelectFromModel01.py
 # @Software: PyCharm 
 # @Comment : 
 import warnings
 
 warnings.filterwarnings('ignore')
-from sklearn.svm import LinearSVC
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.datasets import load_iris
 from sklearn.feature_selection import SelectFromModel
-import numpy as np
 
 iris = load_iris()
 X, y = iris.data, iris.target
-lsvc = LinearSVC(C=0.001, penalty='l1', dual=False).fit(X, y)
-model = SelectFromModel(lsvc, prefit=True)
+clf = ExtraTreesClassifier()
+clf = clf.fit(X, y)
+model = SelectFromModel(clf, prefit=True)
 X_new = model.transform(X)
 print('0' * 100)
 print(X_new.shape)
