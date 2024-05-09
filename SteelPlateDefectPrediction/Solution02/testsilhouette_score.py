18066b42ed7609df1a18c0028c6d055eeb97c093
lixixi89055465 <nanji1850094299@outlook.com>
2024-05-09 17:35:20 +0800

    kaggle

--- SteelPlateDefectPrediction/Solution02/testsilhouette_score.py
+++ SteelPlateDefectPrediction/Solution02/testsilhouette_score.py
@@ -8,4 +8,19 @@
 import warnings
 
 warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+estimator = KMeans(n_clusters=10, random_state=777)
+kmean_data_tf = ""
+estimator.fit(kmean_data_tf)
+r1 = pd.Series(estimator.labels_).value_counts()
+r2 = pd.DataFrame(estimator.cluster_centers_)
+r = pd.concat([r2, r1], axis=1)
+r.columns = list(kmean_data_tf.columns) + [u'类别数据']
+print(r)
+
+print('轮廓系数:', metrics.silhouette_score(kmean_data_tf, estimator.labels_, metrics='euclidean'))
 
