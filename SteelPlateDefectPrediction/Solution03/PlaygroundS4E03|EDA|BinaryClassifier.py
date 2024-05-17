fdb759328499a9b42bc5f0f200629b03e5479fef
nanji <1850094299@qq.com>
2024-05-17 23:33:50 +0800

    kaggle_stu

---
+++ SteelPlateDefectPrediction/Solution03/PlaygroundS4E03|EDA|BinaryClassifier.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/5/17 下午11:30
+# @Author  : nanji
+# @Site    : https://www.kaggle.com/code/ravi20076/playgrounds4e03-eda-binaryclassifier
+# @File    : PlaygroundS4E03|EDA|BinaryClassifier.py
+# @Software: PyCharm 
+# @Comment :
+from gc import collect;
+from warnings import filterwarnings
+filterwarnings('ignore')
+from IPython.display import display_html,clear_output
---
+++ SteelPlateDefectPrediction/Solution03/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/5/17 下午11:32
+# @Author  : nanji
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm 
+# @Comment : 
